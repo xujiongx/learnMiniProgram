69b0adb2d5b246bce2f18e10fd2b4d4e9caaa544
Anbrosex <2499122241@qq.com>
2019-10-09 01:04:55 +0800

    事件

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,25 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+title:['鲁班七号','孙尚香','吕布',],
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleClick(){
+    console.log('点击事件触发')
+  },
+  handleTouchStart(){
+    console.log('触摸事件触发')
+  },
+  handelTouchMove(){
+    console.log('触摸移动事件触发')
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  handEvenClick(event){
+    console.log(event)
+  },
+  handleItemClick(event){
+    // console.log(event)
+    console.log(event.target.dataset.index, event.target.dataset.item)
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
